08f3130f168799a94666527fc5e5ec8caa6a2439
nguyennam2911 <namnhse02675@fpt.edu.vn>
2015-10-29 08:42:39 +0700

    recipe Model

---
+++ DaiLyCook_BackEnd/src/java/model/RecipeModel.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import dao.RecipeDAO;
+import entity.Recipe;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author Nguyen Hoai Nam
+ */
+public class RecipeModel {
+    
+    //get all recipe
+   public List<Recipe> getAllRecipe(){
+       List<Recipe> list = new ArrayList<>();
+       list = RecipeDAO.getInstance().getAllRecipe();
+       return list;
+   }
+}
